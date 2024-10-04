dc5656ef14d4c62da19ac0af686bf78f1b13e9a3
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2024-10-04 15:38:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-async function fetchWords() {
-    const response = await fetch('words.json');
-    const words = await response.json();
-    return words;
-}
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+});
 
-function getRandomWord(words) {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    return words[randomIndex];
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-list a');
+  const currentPath = window.location.pathname.split("/").pop();
 
-async function displayRandomWord() {
-    const words = await fetchWords();
-    const randomWord = getRandomWord(words);
-    
-    document.getElementById('english').textContent = randomWord.English;
-    document.getElementById('finnish').textContent = randomWord.Finnish;
-    document.getElementById('polish').textContent = randomWord.Polish;
-}
-
-window.onload = displayRandomWord;
-
-async function randomizeWord() {
-    const words = await fetchWords();
-    const randomWord = getRandomWord(words);
-
-    setTimeout(() => {
-        document.getElementById('english').textContent = randomWord.English;
-        document.getElementById('finnish').textContent = randomWord.Finnish;
-        document.getElementById('polish').textContent = randomWord.Polish;
-    }, 100);
-}
-
-window.onload = () => {
-    displayRandomWord();
-    document.getElementById('randomize-button').onclick = randomizeWord;
-};
+  navLinks.forEach(link => {
+      const linkPath = link.getAttribute('href');
+      if (linkPath === currentPath) {
+          link.classList.add('active');
+      }
+  });
+});
